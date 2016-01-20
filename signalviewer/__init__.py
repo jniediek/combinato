e31176032b26b3dacc3312c7e84e9f5514eb0102
Johannes Niediek <jonied@posteo.de>
2016-01-20 12:32:55 +0100

    Add __init__.py

--- signalviewer/__init__.py
+++ signalviewer/__init__.py
@@ -2,3 +2,4 @@ from __future__ import absolute_import
 from .manager.tools import debug
 from .manager.man_continuous import H5Manager
 from .options import options
+from .convert.convert_tools import make_blocks
