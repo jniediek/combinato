1c14d6d5118ff5069be8b1b9f8a4e8171d47ff9f
Johannes Niediek <jonied@posteo.de>
2017-06-06 14:46:56 +0200

    Fix python3 issue: add decode

--- combinato/manager/manager_cat.py
+++ combinato/manager/manager_cat.py
@@ -26,7 +26,7 @@ class SortingFile(object):
         self.classes = self.h5fid.root.classes[:]
         self.groups = self.h5fid.root.groups[:]
         self.types = self.h5fid.root.types[:]
-        self.sign = self.h5fid.get_node_attr('/', 'sign')
+        self.sign = self.h5fid.get_node_attr('/', 'sign').decode()
         self.basedir = os.path.dirname(h5fname)
         self.matches = self.h5fid.root.matches[:]
 
@@ -443,7 +443,7 @@ class Combinato(SortingManagerGrouped):
                   'not initialized'.format(sorting_session))
         else:
             self.initialized = True
-        
+
 def test(name, label, ts):
     """
     simple test case, needs a folder as argument
